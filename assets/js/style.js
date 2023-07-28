1992f40cd4a221c96d5e7d426a20074fdf96c8c1
unknown <ksuleymanli99@gmail.com>
2023-07-28 15:33:40 +0300

    JS

---
+++ assets/js/style.js
@@ -0,0 +1,50 @@
+{let a=7;
+let b=5;
+let z=35
+ console.log(z/a);
+}
+
+{ let a=7
+let b=5
+let z=35
+console.log(z/b)
+}
+
+
+{
+    let a=7
+    let b=5
+    let z=1
+    console.log(z*a)
+    }
+   
+    {
+        let a=7
+        let b=5
+        let z=1
+        console.log(z*b)
+    }
+
+    {
+        const a=7
+        const b=5
+        console.log(a)
+    }
+{
+    const a=7
+    const b=5
+    console.log(b)
+}
+
+    // kvadratin perimetri:
+     {
+        let a=4
+    
+        console.log(a*4)
+     }
+
+    //?kvadratin sahesi:
+    {
+        let a=3
+        console.log( a**2  )
+    }
